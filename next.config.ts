ac28753882ce6536dd86e78edaf74990db8ba4e8
bimangunarathne <bimangunarathne@outlook.com>
2025-11-05 12:17:47 +0530

    update the next.config

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProduction = process.env.NODE_ENV === 'production';
-const repoName = 'frsamanthas-workshop';
-
 const nextConfig: NextConfig = {
+  /* config options here */
   reactCompiler: true,
-  
-  
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  basePath: isProduction ? `/${repoName}` : '',
-  assetPrefix: isProduction ? `/${repoName}` : '',
 };
 
 export default nextConfig;
